35455d3cae16ec03ccadeca04fc881f381c5126e
Leena <leenamadaan451@gmail.com>
2020-03-11 17:53:32 +0530

    Revert "Second second second"
    This reverts commit 5159fd72c73ea8fec80fdef5f125d9badda24552.

--- django_project/blog/models.py
+++ django_project/blog/models.py
@@ -13,7 +13,6 @@ class Post(models.Model):
 
     def __str__(self):
         return self.title
-# second
 
     def get_absolute_url(self):
         return reverse('post-detail', kwargs={'pk': self.pk})
