9829516920b6eec6f03e0f6c933a2ae8e19d916d
Leena <leenamadaan451@gmail.com>
2020-03-11 16:27:42 +0530

    All coments removed

--- django_project/blog/models.py
+++ django_project/blog/models.py
@@ -2,12 +2,7 @@ from django.db import models
 from django.utils import timezone
 from django.contrib.auth.models import User
 from django.urls import reverse
-# first commit
-#<<<<<<< HEAD
-# second commit
-# third commit
-# fourth commit
-# fifth commit
+
 
 
 
@@ -16,11 +11,11 @@ class Post(models.Model):
     content = models.TextField()
     #sixth commit
     date_posted = models.DateTimeField(default=timezone.now)
-    author = models.ForeignKey(User, on_delete=models.CASCADE)#Nineth comment
-    # seventh comment
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+
 
     def __str__(self):
         return self.title
 
     def get_absolute_url(self): # sixth commit
-        return reverse('post-detail', kwargs={'pk': self.pk}) # seventh comment
+        return reverse('post-detail', kwargs={'pk': self.pk})
