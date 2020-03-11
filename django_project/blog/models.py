efe3cf69ce82b5d3651b3f4678db07b731a18da0
Leena <leenamadaan451@gmail.com>
2020-03-11 17:05:03 +0530

    All comments removed

--- django_project/blog/models.py
+++ django_project/blog/models.py
@@ -2,30 +2,17 @@ from django.db import models
 from django.utils import timezone
 from django.contrib.auth.models import User
 from django.urls import reverse
-# first commit
-#<<<<<<< HEAD
-# second commit
-<<<<<<< HEAD
-<<<<<<< HEAD
-# third commit
-# fourth commit
-
-=======
->>>>>>> parent of c28554c... Third commit
-=======
->>>>>>> parent of c28554c... Third commit
-
 
 class Post(models.Model):
     title = models.CharField(max_length=100)
     content = models.TextField()
     #sixth commit
     date_posted = models.DateTimeField(default=timezone.now)
-    author = models.ForeignKey(User, on_delete=models.CASCADE)#Nineth comment
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
     # seventh comment
 
     def __str__(self):
         return self.title
 
-    def get_absolute_url(self): # sixth commit
-        return reverse('post-detail', kwargs={'pk': self.pk}) # seventh comment
+    def get_absolute_url(self):
+        return reverse('post-detail', kwargs={'pk': self.pk})
