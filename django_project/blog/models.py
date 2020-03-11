9e852caa2b34efd7a6d33e1900a68baa33626e39
Leena <leenamadaan451@gmail.com>
2020-03-11 17:25:20 +0530

    msg comments removed

--- django_project/blog/models.py
+++ django_project/blog/models.py
@@ -2,19 +2,15 @@ from django.db import models
 from django.utils import timezone
 from django.contrib.auth.models import User
 from django.urls import reverse
-<<<<<<< HEAD
-=======
-# first commit
 
->>>>>>> parent of 91fc787... Second commit
 
 class Post(models.Model):
     title = models.CharField(max_length=100)
     content = models.TextField()
-    #sixth commit
+
     date_posted = models.DateTimeField(default=timezone.now)
     author = models.ForeignKey(User, on_delete=models.CASCADE)
-    # seventh comment
+
 
     def __str__(self):
         return self.title
