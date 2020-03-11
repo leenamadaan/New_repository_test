288cf541815fd5bc1208c4d453f87cc238e15fbb
Leena <leenamadaan451@gmail.com>
2020-03-11 17:34:10 +0530

    Revert "All comments removed"
    This reverts commit efe3cf69ce82b5d3651b3f4678db07b731a18da0.

--- django_project/blog/models.py
+++ django_project/blog/models.py
@@ -3,9 +3,24 @@ from django.utils import timezone
 from django.contrib.auth.models import User
 from django.urls import reverse
 <<<<<<< HEAD
+<<<<<<< HEAD
 =======
 # first commit
 >>>>>>> parent of 91fc787... Second commit
+=======
+# first commit
+#<<<<<<< HEAD
+# second commit
+<<<<<<< HEAD
+<<<<<<< HEAD
+# third commit
+# fourth commit
+
+=======
+>>>>>>> parent of c28554c... Third commit
+=======
+>>>>>>> parent of c28554c... Third commit
+>>>>>>> parent of efe3cf6... All comments removed
 
 
 class Post(models.Model):
@@ -13,11 +28,16 @@ class Post(models.Model):
     content = models.TextField()
 
     date_posted = models.DateTimeField(default=timezone.now)
+<<<<<<< HEAD
     author = models.ForeignKey(User, on_delete=models.CASCADE)
 
+=======
+    author = models.ForeignKey(User, on_delete=models.CASCADE)#Nineth comment
+    # seventh comment
+>>>>>>> parent of efe3cf6... All comments removed
 
     def __str__(self):
         return self.title
 
-    def get_absolute_url(self):
-        return reverse('post-detail', kwargs={'pk': self.pk})
+    def get_absolute_url(self): # sixth commit
+        return reverse('post-detail', kwargs={'pk': self.pk}) # seventh comment
