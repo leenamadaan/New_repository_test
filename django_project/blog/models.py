8a96b55d5f28adf5365b8c086f9466d4ea0fd1f2
Leena <leenamadaan451@gmail.com>
2020-03-12 10:20:05 +0530

    fourth

--- django_project/blog/models.py
+++ django_project/blog/models.py
@@ -6,12 +6,10 @@ from django.urls import reverse
 
 class Post(models.Model):
     title = models.CharField(max_length=100)
-
     content = models.TextField()
-    # third
     date_posted = models.DateTimeField(default=timezone.now)
     author = models.ForeignKey(User, on_delete=models.CASCADE)
-
+# fourth
     def __str__(self):
         return self.title
 
