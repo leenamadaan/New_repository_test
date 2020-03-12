00d91a8613360c3fdb1a765e3e93edc5675f140e
Leena <leenamadaan451@gmail.com>
2020-03-12 10:16:17 +0530

    second

--- django_project/blog/models.py
+++ django_project/blog/models.py
@@ -3,9 +3,10 @@ from django.utils import timezone
 from django.contrib.auth.models import User
 from django.urls import reverse
 
-# first
+
 class Post(models.Model):
     title = models.CharField(max_length=100)
+    # second
     content = models.TextField()
     date_posted = models.DateTimeField(default=timezone.now)
     author = models.ForeignKey(User, on_delete=models.CASCADE)
